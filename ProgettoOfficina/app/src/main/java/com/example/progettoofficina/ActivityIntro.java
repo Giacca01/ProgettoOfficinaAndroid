37dfd45b0be5fca3a9199421c932b31f441b0c03
Giacca01 <fgiacardi01@gmail.com>
2020-05-20 23:15:23 +0200

    "Versione 1.1: Creazione Activity di presentazione"

---
+++ ProgettoOfficina/app/src/main/java/com/example/progettoofficina/ActivityIntro.java
@@ -0,0 +1,25 @@
+package com.example.progettoofficina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityIntro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Button btn = (Button)findViewById(R.id.btnprova);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iForm2 = new Intent(getApplicationContext(), MainActivity.class); //Intent: oggetto che consente di passare da un intent all'altra
+                startActivity(iForm2);
+            }
+        });
+    }
+}
