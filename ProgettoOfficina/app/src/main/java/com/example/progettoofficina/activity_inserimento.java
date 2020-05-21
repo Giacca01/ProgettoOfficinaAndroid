2e706ffa89734aef60b818f551f6c78157cb1d62
Giacca01 <fgiacardi01@gmail.com>
2020-05-21 22:24:53 +0200

    "Versione 1.2: Implementazione Aggiunta Riparazione"

---
+++ ProgettoOfficina/app/src/main/java/com/example/progettoofficina/activity_inserimento.java
@@ -0,0 +1,14 @@
+package com.example.progettoofficina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_inserimento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserimento);
+    }
+}
